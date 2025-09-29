6017f65ebad9fb13501fb0be8821029a800ba0e1
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-09-29 22:38:13 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,12 +7,25 @@ const urlsToCache = [
   "./img/icon-512.png"
 ];
 
+// インストール（キャッシュ登録）
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// 有効化（古いキャッシュ削除）
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// fetch イベントでキャッシュ利用
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
