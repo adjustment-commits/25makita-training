5111f0a9c699c5cb2373d4e211dbb17269cd3b0a
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-09-29 20:55:32 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.jpg"
+  "./icon-192",
+  "./icon-512"
 ];
 
 self.addEventListener("install", event => {
